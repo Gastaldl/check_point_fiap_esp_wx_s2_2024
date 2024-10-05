85df3caf4d922a7fc2d496162203ed1d5500e174
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 21:04:27 -0300

    Adicionando os imports

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -4,10 +4,18 @@
  *      - Márcio Gastaldi   -   RM98811
  *      - Arthur Bessa      -   RM99215
  *      - Davi Desenzi      -   RM550849
- *      - Joâo Victor       -   RM551410
+ *      - João Victor       -   RM551410
  */
 
 package br.com.fiap.twoespwx.libunclepresser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class App 
