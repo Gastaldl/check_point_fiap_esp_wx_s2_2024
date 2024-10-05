2a3ae52724ff45235eb9ce2f2dce222f7629344c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 21:08:00 -0300

    Adicionando pasta para input e output

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -22,6 +22,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("LIB UNCLE PRESSER - GRUPO BATATA-DOCE");
+        System.out.println("LIB UNCLE PRESSER - MIB - Mens In Bussiness");
     }
 }
